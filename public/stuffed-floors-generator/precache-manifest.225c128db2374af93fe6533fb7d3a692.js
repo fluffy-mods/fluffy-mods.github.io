bf18b7130ae7ceb44f6033fc54034eadc1797bbf
Fluffy <fluffy.l2032@gmail.com>
2020-10-01 22:59:10 +0200

    add cra app?

---
+++ public/stuffed-floors-generator/precache-manifest.225c128db2374af93fe6533fb7d3a692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d373e06e18fe832a5a39e1730202f9ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9ed94e299f574122b40",
+    "url": "./static/css/main.b89c04f9.chunk.css"
+  },
+  {
+    "revision": "77481b50dd0f7ffa146a",
+    "url": "./static/js/2.da376b81.chunk.js"
+  },
+  {
+    "revision": "2cdde5ec031c4fb0ff37e2898f9034ab",
+    "url": "./static/js/2.da376b81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9ed94e299f574122b40",
+    "url": "./static/js/main.1d9e9bf2.chunk.js"
+  },
+  {
+    "revision": "95a4ee7a9c06fab6f74a",
+    "url": "./static/js/runtime-main.f7f39704.js"
+  }
+]);
